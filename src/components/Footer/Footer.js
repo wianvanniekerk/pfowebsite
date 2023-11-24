ec59337a1c58b98c3c2b243e8a72e16798703b23
mzaeema <49003750+mzaeema@users.noreply.github.com>
2023-11-24 14:55:42 +0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,28 +4,42 @@ import { AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
 function Footer() {
   return (
     <div>
-      <footer className="fixed_footer">
-        <div className="grid-container">
-          <p className="content">Email:</p>
-          <p className="content">
-            South Africa Office: CSIR Site, Mering Naude Rd, Scientia 627 - Jr,
-            Pretoria
-          </p>
-          <p className="content">
-            North America Office: 6330 South Sandhill Road Las Vegas, NV 89120
-          </p>
-        </div>
-        <div className="makeIcon">
-          <a href="https://www.linkedin.com/company/precision-finops/">
-            <AiFillLinkedin size="2rem" />
-          </a>
-          <a href="https://www.linkedin.com/company/precision-finops/">
-            <AiOutlineMail size="2rem" />
-          </a>
-        </div>
-      </footer>
+      <div class="footer-container">
+		<div class="footer-col">
+			<div>
+				<h4>Contact</h4>
+				<div class="cgroup">
+					<div class="singleitem">
+						info@pfosolutions.com
+					</div>
+					
+					<div class="singlesocial">
+					 <a href="https://www.linkedin.com/company/precision-finops/">
+						<AiFillLinkedin size="2rem" color="white" />
+					  </a>
+					  <a href="https://www.linkedin.com/company/precision-finops/">
+						<AiOutlineMail size="2rem" color="white" />
+					  </a>
+					</div>
+				</div>
+			</div>
+			<div>
+				<h4>Visit Us</h4>
+				<div class="cgroup">
+					<div class="singleaddress">
+						<b>South Africa Office</b><br />
+						CSIR Site, Mering Naude Rd, Scientia 627 - Jr, Pretoria
+					</div>
+					
+					<div class="singleaddress">
+						<b>North America Office</b><br />
+						6330 South Sandhill Road Las Vegas, NV 89120
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
     </div>
   );
 }
-
 export default Footer;
