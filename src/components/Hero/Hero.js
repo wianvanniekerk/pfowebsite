d9256683f5c84d66ab3bc2efd30db8b159dd4374
wianvanniekerk <wianvanniekerk@protonmail.com>
2023-11-24 13:17:25 +0200

    deployed website

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,20 +2,28 @@ import React from "react";
 
 function Hero() {
   return (
-    <div class="hero">
-      <div class="hero__bg">
+    <div className="hero">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Sora:wght@300&display=swap');
+      </style>
+
+      <div className="hero__bg">
         <picture>
-          <img src="/images/Wallmax.png" />
+          <img src="/images/Wallmax.png" alt="Background" />
         </picture>
       </div>
 
-      <div class="hero__cnt">
-        <div class="fade-in">
-              <p>&lt&gt | Precision FinOps</p>
-        </div>
-        <p>Custodians of Logic</p>
+      <div className="hero__cnt">
+        <div className="fade-in">
+          <p>&lt;&gt; | Precision FinOps  </p>
+          <br></br> 
+          <br></br> 
+          <br></br> 
+          <p className="custodians">Custodians of Logic</p>
+        </div>        
       </div>
     </div>
   );
 }
+
 export default Hero;
